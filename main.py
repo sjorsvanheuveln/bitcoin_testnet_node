611d56c5b41b267b07004b6b926cefbec6affff0
Sjors van Heuveln <sjorsvanheuveln@gmail.com>
2021-11-06 17:34:24 +0100

    Working on OP_RETURN outputs

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Author: Sjors van Heuveln 03-11-2021
 from examples import *
 from helpers import *
 from network import *
+from config import *
 
 #basic_node_action(TESTNET_HOST2)
 #send_transaction()
@@ -17,42 +18,52 @@ from network import *
 ####################################
 
 
-#this transaction is messed up
-#try to implement something that can filter this shit.
-# tx_hex_dump = '01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff1d03e50b20045bee726100a0000031010000000a4d696e696e67636f726500000000020000000000000000266a24aa21a9edc003b4a02fa3d44cabbd38d51c7ac73ccf8b6222e1d9993e1d296982f550bf33384a4c00000000001976a914638b34ce24a0cdce5682032fb7edd0402b667dbb88ac00000000'
-# tx_hex_dump2 = '02000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4803c90b200443b47261425443506f6f6cfabe6d6d8cd7b6ff17cf4544ac88396e6f987e4065c50f7699b7b08d519b200532da973b04000000243dccdc0200000eb5773f0000000000ffffffff02f55b4c000000000017a9147bef0b4a4dafa77b2ec52b81659cbcf0d9a91487870000000000000000266a24aa21a9ed3e7a3bbe89632a37506baa53f3aa642e58d9517cc432a08db96c05bd293cb80c00000000'
- 
-# Tx.parse(BytesIO(bytes.fromhex(tx_hex_dump)), testnet=True)
+e = little_endian_to_int(hash256(PASSPHRASE))
+p = PrivateKey(e)
+a = p.point.address(compressed=True, testnet=True)
+#print('my pubkey:', a)
+
+prev_tx = bytes.fromhex('d1e7ad7065b9f2ea10207778fdf224f297915e091c993c4bbeeed4cfaba69661')
+prev_index = 0
+tx_in = TxIn(prev_tx, prev_index)
+
+
+'''TODO
+1. Automate change calculation: use resend address and use feefilter
+2. Try a first OP_RETURN output
+3. Fill it
+4. Multiple outputs
+5. Fill it with art
+6. Try mainnet
+'''
+
+
+
+#change output
+change_amount = int(96000)
+change_h160 = p.point.hash160()
+change_script = p2pkh_script(change_h160)
+change_output = TxOut(amount=change_amount, script_pubkey=change_script)
+
+#target output
+target_amount = int(900)
+target_h160 = decode_base58('mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt')
+target_script = p2pkh_script(target_h160)
+target_output = TxOut(amount=target_amount, script_pubkey=target_script)
+tx_obj = Tx(1, [tx_in], [change_output, target_output], locktime = 0, testnet = True)
+
+
+tx_obj.sign_input(0, p)
+
+node = SimpleNode(TESTNET_HOST3, testnet=True, logging=True)
+node.handshake()
+
+node.send(MempoolMessage())
+inv = node.wait_for(InvMessage)
+print(inv)
 
 
 
 
-# def endian_swap(s):
-#     swaps = ''
-#     for i in range(0, len(s), 2):
-#         swaps += s[i+1] + s[i]
-#     print(swaps[::-1])
-
-
-
-# s = '9b94830000000000'
-# endian_swap(s)
-
-# pubkey = bytes.fromhex('038707dbb5b851f7952e4451ee38859601d3937d0c9260cc3d376f145d425ebea9')
-# print(pubkey)
-# h160 = hash160(pubkey)
-# print(h160.hex())
-# extended_pubkey = b'\x6f' + h160
-
-# # print(h160.hex())
-# #print(extended_pubkey.hex())
-# address = encode_base58_checksum(extended_pubkey)
-# print(address)
-
-# data = bytes.fromhex('2d2d2d424547494e20545249425554452d2d2d20')
-# print(data.decode("ascii"))
-
-len_sassaman_pretty()
-
-
+#node.send(tx_obj)
 
